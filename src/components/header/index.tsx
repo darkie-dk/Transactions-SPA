9e58cc71c462f1b9d09c3f61325f2215d384b303
darkie-dk <kevendaniel.kd@gmail.com>
2025-02-03 16:24:25 -0300

    first commit

---
+++ src/components/header/index.tsx
@@ -0,0 +1,24 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from "@radix-ui/react-dialog";
+import logo from '../../assets/logo.svg'
+import { NewTransactionModal } from "../newTransactionModal";
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logo} />
+
+        <Dialog.Root>
+          <Dialog.DialogTrigger asChild>
+            <NewTransactionButton>New Transaction</NewTransactionButton>
+          </Dialog.DialogTrigger>
+        
+          <NewTransactionModal />
+        </Dialog.Root>
+
+
+      </HeaderContent>
+    </HeaderContainer>
+  )
+}
---
+++ src/utils/formatter.ts
@@ -0,0 +1,6 @@
+export const dateformatter = new Intl.DateTimeFormat('pt-br')
+
+export const priceFormatter = new Intl.NumberFormat('pt-br', {
+  style: 'currency',
+  currency: 'BRL',
+})
